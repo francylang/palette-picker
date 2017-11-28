e9472b7befe336121ec4efde1b566d79d266c46b
Francy Lang <francy.lang@gmail.com>
2017-11-28 14:41:13 -0700

    Generating random colors

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,27 @@
+const generateRandomColor = () => {
+  console.log('in gen color');
+  const characters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += characters[Math.floor(Math.random() * 16)];
+  };
+  return color;
+};
 
+$(document).ready(() => {
+  updateRandomColors();
+});
+
+const updateRandomColors = (i) => {
+  for (var i = 0; i < 6; i++) {
+
+  if(!$(`.color${i}`).hasClass('unlocked-image')) {
+    let color = generateRandomColor()
+    $(`.color${i}`).css('background-color', color);
+    $(`.hex-code${i}`).text(color)
+  };
+ };
+};
+
+
+$('.generate-button').on('click', updateRandomColors)
