3dcb600177503edff89dc1107889d5314c2321a4
Francy Lang <francy.lang@gmail.com>
2017-11-29 15:44:43 -0700

    Post request for projects and palettes

--- server.js
+++ server.js
@@ -48,7 +48,6 @@ app.get('/api/v1/projects/:id/palettes', (request, response) => {
     });
 });
 
-
 // DYNAMIC END POINT
 app.get('/api/v1/palettes/:id', (request, response) => {
   const { id } = request.params;
@@ -68,6 +67,30 @@ app.get('/api/v1/palettes/:id', (request, response) => {
     });
 });
 
+app.get('/api/v1/projects/:id', (request, response) => {
+  const { id } = request.params;
+
+  database('projects').where('id', id).select()
+    .then(project => {
+      if (project.length) {
+        return response.status(200).json(project);
+      } else {
+        return response.status(404).json({
+          error: `Unable to find project with id :${id}`
+        });
+      }
+    })
+    .then(error => {
+      return response.status(500).json({ error });
+    });
+});
+// DELETE palette
+// IF ID DOESNT MATCH:
+// response.status(404).({ error: 'no matching palettes' })
+// IF ID DOES MATCH:
+// response.sendStatus(204)
+
+
 // CREATE A NEW PROJECT
 // if (!project) {
 // return response.status(422).send({
@@ -76,21 +99,51 @@ app.get('/api/v1/palettes/:id', (request, response) => {
 // else - push into an array, and:
 // return response.status(201).json({ id, project })
 
-app.post('/api/vi/palettes', (request, response) => {
-  const { palette } = request.body;
-  // CREATE NEW PALETTE
+app.post('/api/v1/projects', (request, response) => {
+  const project = request.body;
+
+  for (let requiredParameter of ['project_title']) {
+    if (!project[requiredParameter]) {
+      return response.status(422).json({
+        error: `You are missing the ${requiredParameter} property.`
+      });
+    }
+  }
+
+  database('projects').insert(project, 'id')
+    .then(project => {
+      return response.status(201).json({ id: project[0] });
+    })
+    .catch(error => {
+      return response.status(500).json({ error });
+    });
 });
 
-app.delete('/api/v1/palettes/:id', (request, response) => {
-  const { id } = request.params;
+app.post('/api/v1/projects/:id/palettes', (request, response) => {
+  let palette = request.body;
+  const projectId = request.params.id;
 
-  // DELETE palette
-  // IF ID DOESNT MATCH:
-  // response.status(404).({ error: 'no matching palettes' })
-  // IF ID DOES MATCH:
-  // response.sendStatus(204)
+  for (let requiredParameter of ['palette_title', 'hex_code_1', 'hex_code_2', 'hex_code_3', 'hex_code_4', 'hex_code_5']) {
+    if (!palette[requiredParameter]) {
+      return response.status(422).json({
+        error: `You are missing the ${requiredParameter} property.`
+      });
+    }
+  }
 
+  palette = Object.assign({}, palette, { project_id: projectId });
+
+  database('palettes').insert(palette, 'id')
+    .then(palette => {
+      return response.status(201).json({ id: palette[0] });
+    })
+    .catch(error => {
+      return response.status(500).json({ error });
+    });
 });
+
+
+
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`)
