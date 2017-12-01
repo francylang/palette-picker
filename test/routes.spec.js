623b6c2b66d5111b74f83efd63785d986e3326d3
Francy Lang <francy.lang@gmail.com>
2017-11-30 21:50:06 -0700

    happy and sad path

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -4,10 +4,6 @@ const server = require('../server');
 
 const should = chai.should();
 
-const environment = process.env.NODE_ENV || 'test';
-const configuration = require('../knexfile')[environment];
-const database = require('knex')(configuration);
-
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
@@ -17,9 +13,19 @@ describe('Client Routes', () => {
       .then((response) => {
         response.should.have.status(200);
         response.should.be.html;
-        response.res.text.includes('Palette Picker');
+        response.res.text.should.include('Palette Picker');
       })
       .catch((error) => {
+        throw error;
+      });
+  });
+  it('should return a 404 for a route that does not exist', () => {
+    return chai.request(server)
+      .get('/sad')
+      .then(response => {
+        response.should.have.status(404);
+      })
+      .catch(error => {
         throw error;
       });
   });
