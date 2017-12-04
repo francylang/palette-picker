ea2f74c4a7622fcf56b9b131ef63edfa805cd004
Francy Lang <francy.lang@gmail.com>
2017-12-04 09:19:15 -0700

    Add circle configuration

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
+
 const environment = process.env.NODE_ENV || 'test';
 const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
